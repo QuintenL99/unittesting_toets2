f16669630518b71ea8ab6218966b3b37c21c8a70
Quinten Laenen <quinten.laenen@student.kdg.be>
2023-11-13 09:40:05 +0100

    test 7

--- src/test/java/opdracht/LottoFormTest.java
+++ src/test/java/opdracht/LottoFormTest.java
@@ -104,4 +104,23 @@ public class LottoFormTest {
 
         assertTrue(actualMessage.contains(expectedMessage));
     }
+
+    @Test
+    void LottoForm_UNDER_18_IS_FORBIDDEN_Exception(){
+        Set <Integer> numbers = new HashSet<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        LottoForm lottoForm = new LottoForm(numbers);
+
+        Exception exception =  assertThrows(IllegalArgumentException.class, () -> lottoForm.play(null,8));
+
+        String expectedMessage = "Expecting 6 unique numbers + 1 extra number";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 }
