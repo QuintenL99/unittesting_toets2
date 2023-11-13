d38062d8018b0e51f74be8cde07a67a69fc8ffca
Quinten Laenen <quinten.laenen@student.kdg.be>
2023-11-13 08:41:48 +0100

    test 1

--- src/test/java/opdracht/LottoFormTest.java
+++ src/test/java/opdracht/LottoFormTest.java
@@ -1,4 +1,14 @@
 package opdracht;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LottoFormTest {
+
+    @Test
+    void lottoformNullSet() {
+        assertThrows(IllegalArgumentException.class, () -> new LottoForm(null));
+    }
 }
